7a871d755d47778ea69700acf6cdfadbc7b99222
akjj004 <akjj004@gmail.com>
2023-01-01 21:11:06 +0100

    align button

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,8 +3,10 @@ import { Flex, Select, Checkbox, Box, Stack, Icon } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { filterData, getFilterValues } from "../utils/filterData";
 import { baseUrl, fetchApi } from "../pages/api/fetchApi";
+import { Button } from "@chakra-ui/react";
 
 export default function SearchFilters() {
+  const [filterValues, setFilterValues] = useState({});
   const [filters] = useState(filterData);
 
   const router = useRouter();
@@ -25,25 +27,40 @@ export default function SearchFilters() {
   };
 
   return (
-    <Flex bg="blue.300" p="4" justifyContent="center" flexWrap="wrap">
-      {filters?.map((filter) => (
-        <Box key={filter.queryName}>
-          <Select
-            onChange={(e) =>
-              searchProperties({ [filter.queryName]: e.target.value })
-            }
-            placeholder={filter.placeholder}
-            w="fit-content"
-            p="2"
-          >
-            {filter?.items?.map((item) => (
-              <option value={item.value} key={item.value}>
-                {item.name}
-              </option>
-            ))}
-          </Select>
+    <Flex
+      bg="blue.300"
+      p="4"
+      justifyContent="center"
+      alignItems="center"
+      flexWrap="wrap"
+    >
+      <Stack isInline spacing={4}>
+        {filters?.map((filter) => (
+          <Box key={filter.queryName}>
+            <Select
+              onChange={(e) => {
+                const newFilterValues = {
+                  ...filterValues,
+                  [filter.queryName]: e.target.value,
+                };
+                setFilterValues(newFilterValues);
+              }}
+              placeholder={filter.placeholder}
+              w="fit-content"
+              p="2"
+            >
+              {filter?.items?.map((item) => (
+                <option value={item.value} key={item.value}>
+                  {item.name}
+                </option>
+              ))}
+            </Select>
+          </Box>
+        ))}
+        <Box pt={2}>
+          <Button onClick={() => searchProperties(filterValues)}>Search</Button>
         </Box>
-      ))}
+      </Stack>
     </Flex>
   );
 }
